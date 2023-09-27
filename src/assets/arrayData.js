76a6461147bc0e128d95c15712e0c0c865e57106
sebas <carrenosebastian54@gmail.com>
2023-09-27 17:55:08 -0500

    Increase the options range for the pants size field

--- src/assets/arrayData.js
+++ src/assets/arrayData.js
@@ -918,6 +918,15 @@ export const arrayData = [
                     name: 'talla_pantalon',
                     type: 'select',
                     options: [
+                        { value: 6, label: 6 },
+                        { value: 8, label: 8 },
+                        { value: 10, label: 10 },
+                        { value: 12, label: 12 },
+                        { value: 14, label: 14 },
+                        { value: 16, label: 16 },
+                        { value: 18, label: 18 },
+                        { value: 20, label: 20 },
+                        { value: 22, label: 22 },
                         { value: 24, label: 24 },
                         { value: 26, label: 26 },
                         { value: 28, label: 28 },
@@ -925,6 +934,8 @@ export const arrayData = [
                         { value: 32, label: 32 },
                         { value: 34, label: 34 },
                         { value: 36, label: 36 },
+                        { value: 38, label: 38 },
+                        { value: 40, label: 40 },
                     ]
                 },
                 {
